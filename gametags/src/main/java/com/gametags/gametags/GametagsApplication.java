df08d80889242bbd6d1b9ff9788b4a96bcd25d0c
GonzaloLGI <gpson4l0@gmail.com>
2023-04-13 18:20:47 +0200

    test

---
+++ gametags/src/main/java/com/gametags/gametags/GametagsApplication.java
@@ -0,0 +1,13 @@
+package com.gametags.gametags;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GametagsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GametagsApplication.class, args);
+	}
+
+}
---
+++ gametags/src/test/java/com/gametags/gametags/GametagsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gametags.gametags;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GametagsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
