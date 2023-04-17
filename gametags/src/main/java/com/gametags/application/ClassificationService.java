4eb87448963495c17bf5011187bf1038b04130c7
GonzaloLGI <gpson4l0@gmail.com>
2023-04-17 18:25:11 +0200

    feat: added code to methods of classification controller and created classification service

---
+++ gametags/src/main/java/com/gametags/application/ClassificationService.java
@@ -0,0 +1,23 @@
+package com.gametags.application;
+
+import com.gametags.infrastructure.ClassificationDAO;
+
+import java.util.List;
+import java.util.UUID;
+
+public class ClassificationService {
+    public ClassificationDAO createClassification(ClassificationDAO toDomain) {
+    }
+
+    public ClassificationDAO findOneClassification(UUID id) {
+    }
+
+    public List<ClassificationDAO> findAllClassifications() {
+    }
+
+    public ClassificationDAO updateClassification(ClassificationDAO toDomain) {
+    }
+
+    public ClassificationDAO deleteClassification(UUID id) {
+    }
+}
--- gametags/src/main/java/com/gametags/infrastructure/controller/ClassificationController.java
+++ gametags/src/main/java/com/gametags/infrastructure/controller/ClassificationController.java
@@ -1,5 +1,7 @@
 package com.gametags.infrastructure.controller;
 
+import com.gametags.application.ClassificationService;
+import com.gametags.infrastructure.ClassificationDAO;
 import com.gametags.infrastructure.ClassificationDTO;
 import com.gametags.infrastructure.mappers.ClassificationMapper;
 import org.springframework.http.HttpStatus;
@@ -8,39 +10,42 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController("/classification")
 public class ClassificationController {
 
     private ClassificationMapper mapper;
+    private ClassificationService service;
 
     @PostMapping("/")
     @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<ClassificationDTO> createClassification(@RequestBody ClassificationDTO dto){
-        return mapper.toDto(service.createClassification(mapper.toDomain(dto)));
+        return new ResponseEntity<>(mapper.toDto(service.createClassification(mapper.toDomain(dto))),HttpStatus.CREATED);
     }
 
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.FOUND)
     public ResponseEntity<ClassificationDTO> getOneClassification(@PathVariable UUID id){
-
+        return new ResponseEntity<>(mapper.toDto(service.findOneClassification(id)),HttpStatus.FOUND);
     }
 
     @GetMapping("/")
     @ResponseStatus(HttpStatus.FOUND)
     public ResponseEntity<List<ClassificationDTO>> getAllClassification(){
-
+         List<ClassificationDAO> list = service.findAllClassifications();
+         return new ResponseEntity<>(list.stream().map(dao -> mapper.toDto(dao)).collect(Collectors.toList()),HttpStatus.FOUND);
     }
 
     @PutMapping("/")
     @ResponseStatus(HttpStatus.ACCEPTED)
     public ResponseEntity<ClassificationDTO> updateClassification(@RequestBody ClassificationDTO dto){
-
+        return new ResponseEntity<>(mapper.toDto(service.updateClassification(mapper.toDomain(dto))),HttpStatus.ACCEPTED);
     }
 
     @DeleteMapping("/[id}")
     @ResponseStatus(HttpStatus.ACCEPTED)
     public ResponseEntity<ClassificationDTO> deleteClassification(@PathVariable UUID id){
-
+        return new ResponseEntity<>(mapper.toDto(service.deleteClassification(id)),HttpStatus.ACCEPTED);
     }
 }
