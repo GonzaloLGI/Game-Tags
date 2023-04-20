8659d328f254d1afc0a7e1d977a56d6ea547d78c
GonzaloLGI <gpson4l0@gmail.com>
2023-04-20 17:32:25 +0200

    test: adding test for classification mapper

---
+++ gametags/src/test/java/com/gametags/gametags/classification/ClassificationMapper.java
@@ -0,0 +1,14 @@
+package com.gametags.gametags.classification;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class ClassificationMapper {
+
+    @Test
+    public void mapping(){
+        
+    }
+}
